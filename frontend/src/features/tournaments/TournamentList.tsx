8de0c08c9cf02c62a2ea6bcc73c228b158ea00aa
Paul Terry <paul.terry84@gmail.com>
2026-01-23 09:50:44 +0000

    added loading and error states to TournamentList

--- frontend/src/features/tournaments/TournamentList.tsx
+++ frontend/src/features/tournaments/TournamentList.tsx
@@ -4,27 +4,31 @@ import { getTournaments } from "../../api/tournaments"
 
 function TournamentList() {
   const [tournaments, setTournaments] = useState<Tournament[]>([])
+  const [loading, setLoading] = useState<boolean>(true)
+  const [error, setError] = useState<string | null>(null)
 
   useEffect(() => {
     getTournaments()
       .then((tournamentData) => setTournaments(tournamentData))
-      .catch(console.error)
+      .catch((error) => setError(error.message || "An error occurred"))
+      .finally(() => setLoading(false))
   }, [])
 
   const tournamentElements = tournaments.map((tournament) => {
     return (
       <div key={tournament.id}>
         {tournament.name}
-        <p>{tournament.start_date}</p>
-        <p>{tournament.end_date}</p>
+        <p>{new Date(tournament.start_date).toLocaleDateString()}</p>
+        <p>{new Date(tournament.end_date).toLocaleDateString()}</p>
       </div>
     )
   })
 
   return (
     <>
-      <h2>List of Tournaments</h2>
-      {tournamentElements}
+      {loading && <p>Loading...</p>}
+      {!loading && tournamentElements}
+      {error && <p>Error fetching tournaments: {error}</p>}
     </>
   )
 }
